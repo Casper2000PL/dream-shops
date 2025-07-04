7ba9b80dbcccad3e31c073f6b265e18478f41f83
Casper2000PL <kacper.kozlicki2000@gmail.com>
2025-07-04 16:46:55 +0200

    2. Product, Category and Image classes created, MySQL database created

---
+++ src/main/java/com/casper/dreamshops/model/Image.java
@@ -0,0 +1,30 @@
+package com.casper.dreamshops.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.sql.Blob;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Image {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String fileType;
+    private String fileName;
+
+    @Lob
+    private Blob image;
+    private String downloadUrl;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+}
